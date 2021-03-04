70cf3e1a40b20de76f13182dce5a8d3d214f0ce3
Hiago Rios <hiagoriosc@gmail.com>
2021-03-03 21:18:34 -0300

    feat: add springboot initial structure

---
+++ src/main/kotlin/br/uesc/portalacad/PortalacadApplication.kt
@@ -0,0 +1,11 @@
+package br.uesc.portalacad
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortalacadApplication
+
+fun main(args: Array<String>) {
+	runApplication<PortalacadApplication>(*args)
+}
